73f0bfc5f406a561cca3e926a4f27dec1250183d
chetanlovanshi <clc.lovanshi@gmail.com>
2015-12-18 10:18:59 +0530

    Update MyResource.java

--- RestfulApplication/src/main/java/org/chetan/javabrain/RestfulApp/MyResource.java
+++ RestfulApplication/src/main/java/org/chetan/javabrain/RestfulApp/MyResource.java
@@ -13,13 +13,13 @@ public class MyResource {
 
     /**
      * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
+     * to the requestee as "text/plain" media type.
      *
      * @return String that will be returned as a text/plain response.
      */
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "Got it!";
+        return "Got the resource!";
     }
 }
